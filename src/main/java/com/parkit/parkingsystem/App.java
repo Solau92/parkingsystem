bb898077442e47a6b059067ccdb645b75dcf4005
Sophie LAURENT <sophielaurent92@gmail.com>
2023-01-10 16:26:21 +0100

    Branche modification format dates

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,10 +4,11 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+// Modif dates
 public class App {
     private static final Logger logger = LogManager.getLogger("App");
     public static void main(String args[]){
-        logger.info("Initializing Parking System");
+        logger.info("Initializing Parking System") ;
         InteractiveShell.loadInterface();
     }
 }
